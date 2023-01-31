c8b6621c5a4ec5e8472b5c5a448fb7beb290c70b
Camila Pizarro Inostroza <c.pizarro.inostroza@gmail.com>
2023-01-31 02:40:10 -0300

    Fin seccion 10 - Auth

--- index.js
+++ index.js
@@ -10,22 +10,18 @@ const app = express();
 // configurar CORS
 app.use(cors());
 
+// Lectura y parseo del body
+app.use( express.json() );
+
 // Base de datos
 dbConnection();
 
 
-console.log(process.env)
-
 // Rutas
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') )
 
-app.get('/', (req, res) => {
 
-    res.status(400).json({
-        ok: true,
-        msg: 'Hola Mundo'
-    })
-
-});
 
 app.listen( process.env.PORT, () => {
     console.log('servidor corriendo en puerto ' + process.env.PORT);
