c18c8512852051c043b940fb6239d1f7bf2fa31b
Camila Pizarro Inostroza <c.pizarro.inostroza@gmail.com>
2023-02-17 01:05:51 -0300

    Fin seccion 11

--- models/usuario.js
+++ models/usuario.js
@@ -31,9 +31,7 @@ const UsuarioSchema = Schema({
 
 UsuarioSchema.method('toJSON', function() {
     const { __v, _id, password, ...object} = this.toObject();
-
     object.uid = _id;
-
     return object;
 })
 
